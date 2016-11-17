7618c1f81d2e653aff76f8d61135b8ae810c615d
Eddie <eddie.jostell@gmail.com>
2016-11-17 12:12:06 +0100

    Started on the food app

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var myMain = document.getElementsByTagName("main");
+var newDiv = document.createElement("div");
+newDiv.setAttribute("class", "randomMealDiv");
+newDiv.style.width = "100%";
+newDiv.style.height = "300px";
+newDiv.style.backgroundColor = "red";
+myMain.appendChild(newDiv);
+
