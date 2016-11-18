312b9decffc74607774f66bf680889b8c116dc1c
Eddie <eddie.jostell@gmail.com>
2016-11-18 02:02:08 +0100

    fixed all the visual shit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-var myMain = document.getElementsByTagName("main");
-var newDiv = document.createElement("div");
-newDiv.setAttribute("class", "randomMealDiv");
-newDiv.style.width = "100%";
-newDiv.style.height = "300px";
-newDiv.style.backgroundColor = "red";
-myMain.appendChild(newDiv);
 
+/// WHAT YOU WANT FOR DIN DINS?
+
+var foodArray = [];
+
+function foodNetwork(name, url, veg) {
+    this.name = name;
+    this.url = url;
+    this.veg = veg;
+}
+
+var goBtn = document.getElementById("foodBtn");
+
+goBtn.addEventListener("click", showFood);
+
+function showFood(){
+    alert("I AM THE BEST AROUND!");
+}
+
+
+/*addfoodNetwork(kottbullar=new foodNetwork("Köttbullar med potatismos, gräddsås, lingon och stektlök.", false,"https://www.landleyskok.se/recept/hemlagade-kottbullar.html/"));
+addfoodNetwork(kolbulle = new foodNetwork("Grillad kolbulle med lingonsylt.", false, "http://www.koket.se/sommar-med-ernst/ernst-kirchsteiger/kolbulle-pa-ernsts-vis/"));
+addfoodNetwork(springroll= new foodNetwork("Vårrulle med sötsursås.", true, "http://www.ica.se/recept/farska-varrullar-med-grillad-tofu-och-ingefarsdipp-720099/"));
+addfoodNetwork(haloumi= new foodNetwork("Haloumisallad", true, "http://mittkok.expressen.se/recept/halloumisallad/"));
+addfoodNetwork(Baconinlindad=new foodNetwork("Baconinlindad kycklingfile.",false,"https://www.kokaihop.se/recept/baconinlindad-kycklingfile-2"));
+addfoodNetwork(lasagne=new foodNetwork("Lasagne med köttfärssås.",false,"http://www.koket.se/lasagne-med-kottfarssas"));
+addfoodNetwork(majs=new foodNetwork("Majsplättar med avokadoröra.",true,"https://www.mathem.se/recept/mattias-majsplattar-med-avokado-chimichurri"));
+addfoodNetwork(soppachori=new foodNetwork("Grönkålssoppa med chorizo.",false,"http://www.matklubben.nu/recept/gr%F6nk%E5lssoppa_med_chorizo_122704.html#.WCxTNcn3b9I"));
+addfoodNetwork(pasta=new foodNetwork("Pastagratäng med rökt lax och spenat.",false,"www.ica.se/recept/pastagratang-med-rokt-lax-och-spenat-719271/"));
+addfoodNetwork(thaigryta=new foodNetwork("Thaigryta med kyckling.",false,"http://www.koket.se/mitt-kok/donal-skehan/thaigryta-med-kyckling/"));
+addfoodNetwork(lax=new foodNetwork("Lax med nudelsallad.",false,"https://www.mathem.se/recept/gustavs-sesamstekta-lax-med-varm-nudelsallad--lime-och-koriander"));
+addfoodNetwork(lasagnehall=new foodNetwork("Lasagne med halloumi, spenat och pumpakärnor.",true,"www.ica.se/recept/lasagne-med-halloumi-spenat-och-pumpakarnor-718382/"));
+addfoodNetwork(risotto=new foodNetwork("Rödbetsrisotto med hyvlad halloumi och valnötter.",true,"www.ica.se/recept/rodbetsrisotto-med-hyvlad-halloumi-och-valnotter-720045/"));
+addfoodNetwork(tonfisk=new foodNetwork("Halstrad tonfisk med avokadohummus.",false,"www.ica.se/recept/halstrad-tonfisk-med-avokadohummus-719975/"));
+addfoodNetwork(skav=new foodNetwork("Viltskavsgryta med mandelpotatispuré & rårörda lingon.",false,"https://www.mathem.se/recept/viltskavsgryta-med-mandelpotatispure-o-rarorda-lingon"));
+addfoodNetwork(coq=new foodNetwork("Coq Au Vin.",false,"http://www.foodnetwork.com/recipes/ina-garten/coq-au-vin-recipe4.html"));
+addfoodNetwork(dahl=new foodNetwork("Indisk linssoppa ”Mulligatawny”.",true,"http://www.ica.se/recept/indisk-linssoppa-mulligatawny-718554/"));
+addfoodNetwork(Falafel=new foodNetwork("Falafel med hummus, yoghurtdressing och picklad rödlök.",true,"http://www.ica.se/recept/falafel-med-hummus-yoghurtdressing-och-picklad-rodlok-717166/"));
+addfoodNetwork(pastag=new foodNetwork("Vegetarisk pastagratäng.",true,"https://www.landleyskok.se/recept/vegetarisk-pastagratang.html/"));
+*/
